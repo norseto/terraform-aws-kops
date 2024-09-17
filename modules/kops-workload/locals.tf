87849e97784516276c7b9c6e22af4edd4e01c7e5
Norihiro Seto <norseto@protonmail.com>
2024-09-17 17:55:50 +0000

    Workload fix

--- modules/kops-workload/locals.tf
+++ modules/kops-workload/locals.tf
@@ -1,10 +1,10 @@
 locals {
   cluster_name                     = var.cluster_name
   cluster                          = data.kops_cluster.cluster
-  vpc_id                           = local.cluster.network_id
-  zone                             = local.cluster.subnet[0].zone
+  vpc_id                           = local.cluster.networking[0].network_id
+  zone                             = local.cluster.networking[0].subnet[0].zone
   region                           = data.aws_availability_zone.zone.region
-  managed_load_balancer_controller = try(local.cluster.aws_load_balancer_controller[0].enabled, false)
+  managed_load_balancer_controller = try(local.cluster.cloud_provider[0].aws[0].load_balancer_controller[0].enabled, false)
   managed_cert_manager             = try(local.cluster.cert_manager[0].managed, false)
 
   install = var.installation
